a35b6fd9f2a15a92c2b6afb556e877fc60080c59
Smeshko606 <smeskolisicka@gmail.com>
2024-11-20 17:09:57 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let temperatura = 25;
+let fahrenheit = (temperatura * 9) / 5 + 32;
+console.log(fahrenheit);
+
+let daysInMonth = 30;
+let hoursInMonth = daysInMonth * 24;
+let minutesInMonth = hoursInMonth * 60;
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+let health = 100;
+let energy = 80;
+health -= 20;
+energy -= 15;
+console.log(health);
+console.log(energy);
+
+let amount = 200;
+let discount = 0.1;
+let summasale = amount * (1 - discount);
+console.log(summasale);
+
+let Number = 7.8;
+let summaNumber = Math.floor(Number);
+console.log(summaNumber);
+
+let chislo = "3.14";
+let summachislo = parseFloat(chislo);
+console.log(summachislo);
+
+let string = "42";
+let summa = parseInt(string);
+console.log(summa);
+
+let number = 16;
+let String = Math.sqrt(number);
+console.log(String);
+
+let x = 10;
+let b = "20";
+let parse = parseInt(b);
+console.log(parse);
+let math = x.toString();
+console.log(math);
